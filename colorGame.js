b96dad287fd91d882c6ac2cfec61e7e7b89f83a8
Jackie <jackiechoi66@gmail.com>
2017-03-22 12:24:22 +0100

    Click event, random number generator, message display added

--- colorGame.js
+++ colorGame.js
@@ -1,4 +1,4 @@
-var sixColorsArray = generateRandomColors();
+var sixColorsArray = generateSixColors();
 var squares = document.querySelectorAll(".square");
 
 var promptColor = generatePromptColor(); //pickColor
@@ -13,6 +13,7 @@ for(var i=0; i < squares.length; i++){
 	//add click listeners to squares
 	squares[i].addEventListener('click', function(){
 		var clickedColor = this.style.background;
+		console.log("clickedColor: "+clickedColor)
 		//if the clickedColor is equal to colorDisplay, user wins
 		if(clickedColor===promptColor){
 			//alert('Correct!')
@@ -25,7 +26,7 @@ for(var i=0; i < squares.length; i++){
 		}
 	})
 }
-
+//when correctly guessed, change colors
 function changeColors(color){
 	//loop thru all squares
 	for(var i=0; i<squares.length; i++){
@@ -33,20 +34,23 @@ function changeColors(color){
 		squares[i].style.background = color;
 	}
 }
+
 //pick one of 6 random colors
 function generatePromptColor(){ //pickColor
 	//random number between 1 and 6
 	var oneThruSix = Math.floor(Math.random()*7);
+	console.log("sixColorsArray[oneThruSix]: "+sixColorsArray[oneThruSix])
 	return sixColorsArray[oneThruSix];
 }
 //create an array of 6 rgb's
-function generateRandomColors(){
+function generateSixColors(){
 	//empty array
 	var arr = [];
 	for(var i=0; i<6; i++){
 		//get random colors 6 times and push to array
 		arr.push(randomColor())
 	}
+	console.log("sixColorsArray: "+arr)
 	return arr;
 }
 //get a random rgb
@@ -54,7 +58,8 @@ function randomColor(){
 	var r = Math.floor(Math.random()*256)
 	var g = Math.floor(Math.random()*256)
 	var b = Math.floor(Math.random()*256)
-	return "rgb("+r+","+g+","+b+")";
+	console.log("rgb("+r+", "+g+", "+b+")")
+	return "rgb("+r+", "+g+", "+b+")";
 
 }
 
